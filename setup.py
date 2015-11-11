330cfb1d979252d6cd63bee6e73104b85eb09b57
Aaron Madison <aaron.l.madison@gmail.com>
2015-11-11 13:38:46 -0600

    Peripheral: Differentiated xml output based on python version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Convert Objects and Django models to/from fixed format records.",
     url="https://github.com/imtapps/django-copybook",
     long_description=open('README.txt', 'r').read(),
-    packages=find_packages(exclude=["example"],),
+    packages=find_packages(),
     install_requires=['six'],
     zip_safe=False,
     classifiers=[
@@ -19,6 +19,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
         "Topic :: Software Development :: Libraries :: Python Modules",
--- test_djcopybook.py
+++ test_djcopybook.py
@@ -6,8 +6,9 @@ from xmlrunner import XMLTestRunner
 
 
 if __name__ == "__main__":
+    py_version = "v{0}{1}".format(*sys.version_info[:2])
     test_suite = unittest.TestLoader().discover("djcopybook")
-    runner = XMLTestRunner(verbosity=2, output="jenkins_reports")
+    runner = XMLTestRunner(verbosity=2, output="jenkins_reports", outsuffix=py_version)
     result = runner.run(test_suite)
 
     sys.exit(not result.wasSuccessful)
