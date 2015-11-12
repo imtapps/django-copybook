e9242da23e82b336f1a99c07459bd4583bf1a50b
Aaron Madison <aaron.l.madison@gmail.com>
2015-11-12 13:51:26 -0600

    Version: 1.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-copybook",
-    version='1.2.0',
+    version='1.2.1',
     author="imtapps",
     author_email="webadmin@imtapps.com",
     description="Convert Objects and Django models to/from fixed format records.",
